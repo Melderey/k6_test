125c678d7adf73fe0b43de0d39be42e2ae260067
Melderey <mimeev2010@gmail.com>
2024-03-04 21:00:56 +0500

    Add convert har file to js

---
+++ har_to_js/05_Select_results.js
@@ -0,0 +1,54 @@
+/*
+ * Creator: Firefox 122.0.1
+ * Browser: Firefox 122.0.1
+ */
+
+import { sleep, group } from 'k6'
+import http from 'k6/http'
+
+export const options = {}
+
+export default function main() {
+  let response
+
+  group('page_5 - http://localhost:1080/webtours/', function () {
+    response = http.post(
+      'http://localhost:1080/cgi-bin/reservations.pl',
+      {
+        outboundFlight: '021;301;02/13/2024',
+        returnFlight: '201;301;03/14/2024',
+        numPassengers: '1',
+        advanceDiscount: '0',
+        seatType: 'Coach',
+        seatPref: 'None',
+        'reserveFlights.x': '76',
+        'reserveFlights.y': '6',
+      },
+      {
+        headers: {
+          Host: 'localhost:1080',
+          'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
+          Accept:
+            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+          'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
+          'Accept-Encoding': 'gzip, deflate, br',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Origin: 'http://localhost:1080',
+          Connection: 'keep-alive',
+          Referer: 'http://localhost:1080/cgi-bin/reservations.pl',
+          Cookie:
+            'MSO=SID&1707745210; MTUserInfo=firstName&Jojo&address2&&username&jojo&hash&47&lastName&Bean%0A&address1&&creditCard&&expDate&%0A',
+          'Upgrade-Insecure-Requests': '1',
+          'Sec-Fetch-Dest': 'frame',
+          'Sec-Fetch-Mode': 'navigate',
+          'Sec-Fetch-Site': 'same-origin',
+          'Sec-Fetch-User': '?1',
+        },
+      }
+    )
+  })
+
+  // Automatically added sleep
+  sleep(1)
+}
